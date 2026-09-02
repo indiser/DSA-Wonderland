36a8c85303a5f2b26f65c14562ad8530fce3d54f
indiser <banerjeerana74@gmail.com>
2026-09-02 21:26:52 +0530

    02/06/2026

--- c++ programming/Daily/3875.cpp
+++ c++ programming/Daily/3875.cpp
@@ -9,35 +9,9 @@ using namespace std;
 class Solution
 {
     public:
-        bool oddEven(int num)
-        {
-            if(num % 2 == 0) return true;
-            return false;
-        }
         bool uniformArray(vector<int> &nums1)
         {
-            int n = nums1.size();
-
-            vector<int> nums2(n, 0);
-
-            int i = 0, j = n - 1;
-
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = i + 1; j < n; j++)
-                {
-                    nums2[i] = nums1[i] - nums2[j];
-                    if(oddEven(nums2[i])) return true; 
-                }
-            }
-
-            for (int i = 0; i < n; i++)
-            {
-                if(oddEven(nums2[i])) return true;
-            }
-            
-            
-            return false;
+            return true;
         }
 };
 
